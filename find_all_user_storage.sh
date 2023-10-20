7170d75c519dee45303faef672ddaef3f7a8d8d1
tang584 <meng.tang@pnnl.gov>
2023-10-20 14:50:47 -0700

    check overhead for root and mount

--- find_all_user_storage.sh
+++ find_all_user_storage.sh
@@ -12,10 +12,8 @@ declare -A directories
 # The '-group user' option specifies that we want directories owned by the 'user' group
 # The '-maxdepth 2' option limits the search to a maximum depth of 2 levels
 
-loop_count=0 # Track searching overhead
-
-
 # ---- Find Root Directories
+loop_count=0 # Track searching overhead
 while IFS= read -r -d '' dir; do
     # Extract the parent directory (up to the second level)
     parent_dir="$(dirname "$dir")"
@@ -23,7 +21,7 @@ while IFS= read -r -d '' dir; do
     directories["$parent_dir"]=1
     let loop_count++
 done < <(find / -maxdepth 2 -type d -group $user -print0 2>/dev/null)
-
+echo "checked paths for root: $loop_count"
 
 # ---- Find Mounted Directories
 exclude_paths=("/sys" "/dev" "/proc" "/run" "/var")
@@ -31,6 +29,7 @@ exclude_paths=("/sys" "/dev" "/proc" "/run" "/var")
 # Join the array elements with the | character as the delimiter
 exclude_pattern=$(IFS="|"; echo "${exclude_paths[*]}")
 
+loop_count=0 # Track searching overhead
 while IFS= read -r dir; do
     # Store the parent directory path in the associative array to ensure uniqueness
     directories["$dir"]=1
@@ -38,7 +37,7 @@ while IFS= read -r dir; do
 done < <(findmnt --noheadings --list | grep -Ev "$exclude_pattern" | cut -d ' ' -f1)
 # 'findmnt --uniq' option works up to findmnt from util-linux 2.32.1
 
-echo "path checked: $loop_count"
+echo "checked paths for mount: $loop_count"
 
 
 
