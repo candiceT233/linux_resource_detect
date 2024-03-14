dec057961e038714be9898550eaf93a9b377c9ca
MengTang <candicetang233@gmail.com>
2024-03-13 22:38:00 -0500

    update run script to change input number

--- io_test/run_seismic_ramdisk.sh
+++ io_test/run_seismic_ramdisk.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-CONCURRENCY=$1 # test 5 10 20
-
-# Check user input
-if [ -z "$CONCURRENCY" ]; then
-    echo "Usage: $0 <concurrency>"
-    exit 1
-fi
-
 IterDecon_BIN=///qfs/people/tang584/scripts/linux_resource_detect/example_workflow/seismology-workflow/bin
 EXP_DATA_PATH=///dev/shm/$USER/seismic_data # Local SSD
 MSHOCK_DATA_PATH=$EXP_DATA_PATH/MShock
 EGF_INPUT_PATH=$EXP_DATA_PATH/EGF
 
+CONCURRENCY=$1 # test 5 10 20
+INPUT_FILE_NUM=$2
+
+# Check user input
+if [ -z "$CONCURRENCY" ]; then
+    echo "Usage: $0 <concurrency> <input_file_num>"
+    exit 1
+fi
+
 mkdir -p $EXP_DATA_PATH
+cd $EXP_DATA_PATH
 
 # cleanup data
-rm -rf $$EXP_DATA_PATH/*.stf
-rm -rf $$EXP_DATA_PATH/*good-fit*
+rm -rf $EXP_DATA_PATH/*.stf
+rm -rf $EXP_DATA_PATH/*good-fit*
 
 # record start time in milliseconds
 time_1=$(($(date +%s%N)/1000000))
 echo "Start sG1IterDcon --------------------------------"
 
-all_input_prefix=( "b916-pb-_ldsp" "g43a-ta-_ldsp" "d27-xt-_ldsp" "nc05-xq-_ldsp" "q43a-ta-_ldsp" "ss64-xi-_ldsp" "enh-ic-00_ldsp" "ss72-xi-_ldsp" "maja-xv-_ldsp" "n02d-ta-_ldsp" "i55a-ta-_ldsp" "149a-ta-_ldsp" "bar-ci-_ldsp" "dac-lb-_ldsp" "d34-xt-_ldsp" "pats-ps-_ldsp" "tato-iu-10_ldsp" "gugu-xf-_ldsp" "b026-pb-_ldsp" "frb-cn-_ldsp")
+readarray -t all_input_prefix < <(head -n $INPUT_FILE_NUM all_seismic_input.txt)
+# echo ${all_input_prefix[@]}
+
+# all_input_prefix=( "b916-pb-_ldsp" "g43a-ta-_ldsp" "d27-xt-_ldsp" "nc05-xq-_ldsp" "q43a-ta-_ldsp" "ss64-xi-_ldsp" "enh-ic-00_ldsp" "ss72-xi-_ldsp" "maja-xv-_ldsp" "n02d-ta-_ldsp" "i55a-ta-_ldsp" "149a-ta-_ldsp" "bar-ci-_ldsp" "dac-lb-_ldsp" "d34-xt-_ldsp" "pats-ps-_ldsp" "tato-iu-10_ldsp" "gugu-xf-_ldsp" "b026-pb-_ldsp" "frb-cn-_ldsp")
+
+num_files="${#all_input_prefix[@]}"
 
 for t in {1..20}; do
     input_prefix=${all_input_prefix[$t-1]}
@@ -57,7 +64,12 @@ time_2=$(($(date +%s%N)/1000000))
 
 echo "Start siftSTFByMisfit.py --------------------------------"
 
-python $IterDecon_BIN/siftSTFByMisfit.py $EXP_DATA_PATH/b916-pb-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/g43a-ta-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/d27-xt-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/nc05-xq-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/q43a-ta-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/ss64-xi-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/enh-ic-00_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/ss72-xi-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/maja-xv-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/n02d-ta-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/i55a-ta-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/149a-ta-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/bar-ci-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/dac-lb-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/d34-xt-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/pats-ps-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/tato-iu-10_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/gugu-xf-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/b026-pb-_ldsp.lht_iter_g1.stf $EXP_DATA_PATH/frb-cn-_ldsp.lht_iter_g1.stf
+# Get all .stf files in the directory #EXP_DATA_PATH into a string
+file_str=$(ls $EXP_DATA_PATH | grep ".stf" | tr '\n' ' ')
+echo "Files: $file_str"
+
+python $IterDecon_BIN/siftSTFByMisfit.py $file_str
+
 
 time_3=$(($(date +%s%N)/1000000))
 
